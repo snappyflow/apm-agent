67b84fe545887c3b22cefd6a445870d6e9d6b622
deepamartin <deepa.martin@maplelabs.com>
2021-08-17 12:28:16 +0530

    Instead of appending lines for every PATH variables changed it to replace exisitng line

--- install.sh
+++ install.sh
@@ -253,7 +253,7 @@ then
     done
 fi
 echo "Environment PATH=$DEFAULTPATH"
-echo "PATH=$DEFAULTPATH" >> $AGENTDIR/env.conf
+echo "PATH=$DEFAULTPATH" > $AGENTDIR/env.conf
 
 if [ ! -z "$ENV_VARS" ]
 then
